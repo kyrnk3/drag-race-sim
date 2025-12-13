7ea66f9dd285f6d04fc9e98c19b5a533fa754479
kyrnk3 <kyrnk3@gmail.com>
2025-12-13 03:18:27 -0800

    Create queens.js

---
+++ queens.js
@@ -0,0 +1,197 @@
+// queens_default.js
+// Auto-generated from queens.json
+
+window.DEFAULT_QUEENS = [
+  {
+    "name": "Cherry Bomb",
+    "stats": {
+      "runway": 8,
+      "comedy": 6,
+      "acting": 7,
+      "dance": 5,
+      "singing": 4,
+      "improv": 7
+    },
+    "traits": {
+      "drama_prone": true,
+      "producer_fave": false
+    },
+    "edits": ["fan_favorite", "underestimated"]
+  },
+  {
+    "name": "Velvet Noir",
+    "stats": {
+      "runway": 9,
+      "comedy": 4,
+      "acting": 6,
+      "dance": 6,
+      "singing": 5,
+      "improv": 5
+    },
+    "traits": {
+      "drama_prone": false,
+      "producer_fave": false
+    },
+    "edits": ["front_runner"]
+  },
+  {
+    "name": "Baby Blue",
+    "stats": {
+      "runway": 6,
+      "comedy": 8,
+      "acting": 5,
+      "dance": 7,
+      "singing": 6,
+      "improv": 8
+    },
+    "traits": {
+      "drama_prone": true,
+      "producer_fave": false
+    },
+    "edits": ["late_bloomer"]
+  },
+  {
+    "name": "Saint Mirage",
+    "stats": {
+      "runway": 7,
+      "comedy": 5,
+      "acting": 8,
+      "dance": 4,
+      "singing": 7,
+      "improv": 6
+    },
+    "traits": {
+      "drama_prone": false,
+      "producer_fave": false
+    },
+    "edits": ["underestimated"]
+  },
+  {
+    "name": "Electra Shock",
+    "stats": {
+      "runway": 5,
+      "comedy": 6,
+      "acting": 5,
+      "dance": 9,
+      "singing": 6,
+      "improv": 5
+    },
+    "traits": {
+      "drama_prone": false,
+      "producer_fave": false
+    },
+    "edits": ["lip_sync_assassin", "messy"]
+  },
+  {
+    "name": "Mysteria Moore",
+    "stats": {
+      "runway": 7,
+      "comedy": 7,
+      "acting": 6,
+      "dance": 6,
+      "singing": 8,
+      "improv": 7
+    },
+    "traits": {
+      "drama_prone": false,
+      "producer_fave": true
+    },
+    "edits": ["front_runner"]
+  },
+  {
+    "name": "Opal Horizon",
+    "stats": {
+      "runway": 9,
+      "comedy": 3,
+      "acting": 6,
+      "dance": 5,
+      "singing": 6,
+      "improv": 4
+    },
+    "traits": {
+      "drama_prone": false,
+      "producer_fave": false
+    },
+    "edits": ["robbed"]
+  },
+  {
+    "name": "Disco Vixen",
+    "stats": {
+      "runway": 7,
+      "comedy": 5,
+      "acting": 5,
+      "dance": 9,
+      "singing": 7,
+      "improv": 6
+    },
+    "traits": {
+      "drama_prone": true,
+      "producer_fave": false
+    },
+    "edits": ["lip_sync_assassin", "messy"]
+  },
+  {
+    "name": "Molly Mocktail",
+    "stats": {
+      "runway": 6,
+      "comedy": 9,
+      "acting": 7,
+      "dance": 5,
+      "singing": 5,
+      "improv": 9
+    },
+    "traits": {
+      "drama_prone": true,
+      "producer_fave": false
+    },
+    "edits": ["villain", "fan_favorite"]
+  },
+  {
+    "name": "Ivory Anthem",
+    "stats": {
+      "runway": 7,
+      "comedy": 4,
+      "acting": 6,
+      "dance": 6,
+      "singing": 9,
+      "improv": 5
+    },
+    "traits": {
+      "drama_prone": false,
+      "producer_fave": true
+    },
+    "edits": ["late_bloomer"]
+  },
+  {
+    "name": "Pixel Pixie",
+    "stats": {
+      "runway": 5,
+      "comedy": 8,
+      "acting": 6,
+      "dance": 6,
+      "singing": 4,
+      "improv": 8
+    },
+    "traits": {
+      "drama_prone": true,
+      "producer_fave": false
+    },
+    "edits": ["messy", "fan_favorite"]
+  },
+  {
+    "name": "Crimson Crown",
+    "stats": {
+      "runway": 8,
+      "comedy": 5,
+      "acting": 8,
+      "dance": 7,
+      "singing": 7,
+      "improv": 6
+    },
+    "traits": {
+      "drama_prone": false,
+      "producer_fave": false
+    },
+    "edits": ["front_runner", "robbed"]
+  }
+];
